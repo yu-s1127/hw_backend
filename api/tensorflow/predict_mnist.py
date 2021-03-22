6aac508ebaea64011fd6216809fca4a7d29f3cf7
s-y1127 <yooopopo5@gmail.com>
2021-03-23 04:53:02 +0900

    changed the predict api

--- api/tensorflow/predict_mnist.py
+++ api/tensorflow/predict_mnist.py
@@ -1,8 +1,12 @@
 import tensorflow as tf
 import numpy as np
 
-def predict(image):
-    loaded_model = tf.keras.models.load_model("api/tensorflow/saved_models/simple_model.h5")
+def predict(image, model_type):
+    if model_type == "auged":
+        model_name = "augmented_model"
+    else:
+        model_name = "simple_model"
+    loaded_model = tf.keras.models.load_model(f"api/tensorflow/saved_models/{model_name}.h5")
     prediction = loaded_model.predict(image)
     predicted_label = np.argmax(prediction)
     prediction_prob = prediction[0].tolist()
