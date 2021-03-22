d59545133d50c5afaa18b355c8aca9c580a374c2
s-y1127 <yooopopo5@gmail.com>
2021-03-23 02:43:14 +0900

    created predict api

--- api/views.py
+++ api/views.py
@@ -1,11 +1,24 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from .tensorflow import image_preprocessing
+from .tensorflow import predict_mnist
+
 import json
 
 @csrf_exempt
 def predict(request):
     body = json.loads(request.body)
-    print(body)
+    base64_string = body["image"].split(",")[1]
 
-    return HttpResponse(status=200)
+    numpy_image = image_preprocessing.base64_to_numpy(base64_string)
+    predicted_label, prediction_prob = predict_mnist.predict(numpy_image)
+
+    response_body = {
+        "predicred_label": str(predicted_label),
+        "prediction_prob": prediction_prob
+    }
+
+    response_json = json.dumps(response_body, ensure_ascii=False, indent=2)
+
+    return HttpResponse(response_json, status=200)
